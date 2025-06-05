cd6f1d7e1e5ccb2424929099b98d87a2cee4a799
noxious <rahman.rafiur.8132@gmail.com>
2025-06-05 18:51:07 +0600

    Remove old CRA/Webpack client

--- client/src/components/DebtDetails.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const DebtDetails = () => {
-  return (
-    <div>DebtDetails</div>
-  )
-}
-
-export default DebtDetails
--- client/src/components/MessageComponent.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const MessageComponent = () => {
-  return (
-    <div className='text-md text-gray-300 my-10 flex justify-center'>Coming Soon</div>
-  )
-}
-
-export default MessageComponent
